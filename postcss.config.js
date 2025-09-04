736bd05e5c35ccb4d944b16974991ddd43bc27d6
Stan Gal <stangal@Stans-Air.sunrise.box>
2025-09-04 10:44:06 +0200

    tailwindv4

--- postcss.config.js
+++ postcss.config.js
@@ -1,2 +1,7 @@
 // postcss.config.js
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } }
+module.exports = {
+    plugins: {
+      '@tailwindcss/postcss': {},   // 👈 v4 plugin (NOT "tailwindcss")
+      autoprefixer: {},
+    },
+  };
